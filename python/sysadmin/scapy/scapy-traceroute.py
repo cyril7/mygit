d2c20e7d22d02aba37a5de7e601653109d3807c3
cyril7 <ericpenguins@gmail.com>
2015-04-11 12:36:09 +0800

    scapy add

---
+++ python/sysadmin/scapy/scapy-traceroute.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+# yum install -y  graphviz ImageMagick 
+# pip install scapy
+"""
+
+import os, sys, time, subprocess
+import warnings, logging
+
+# 屏蔽scapy无用告警信息
+warnings.fiterwarnings("ignore", category = DeprecationWarning)
+
+loggin
+
