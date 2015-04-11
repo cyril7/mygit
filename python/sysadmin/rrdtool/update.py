3f14ab5b87796b8f4f02ab937e0cf9e49f054e5d
cyril7 <ericpenguins@gmail.com>
2015-04-11 12:19:12 +0800

    rrdtool cmd

--- python/sysadmin/rrdtool/update.py
+++ python/sysadmin/rrdtool/update.py
@@ -16,4 +16,10 @@ print update
 
 """
 */5 * * * * /usr/bin/python /tmp/rrdtool/update.py > /dev/null 2>&1
+
+rrdtool info flow.rrd          : 查看rrd文件的结构信息
+rrdtool first flow.rrd         : 查看rrd文件第一个数据的更新时间
+rrdtool last flow.rrd          : 查看rrd文件最后一次更新的时间
+rrdtool fetch flow.rrd AVERAGE : fetch 根据指定时间,CF查询rrd文件
+
 """
