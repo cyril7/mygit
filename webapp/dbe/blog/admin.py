cdbeceadb931a49a94cebe3f6522d44684a94274
criver <ericpenguins@gmail.com>
2015-04-13 23:53:23 +0800

    cyril7 add webapp dbe

---
+++ webapp/dbe/blog/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+from blog.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ['title']
+
+admin.site.register(Post, PostAdmin)
+
---
+++ webapp/dbe/dbe/urls.py
@@ -0,0 +1,28 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'dbe.views.home', name='home'),
+    # url(r'^dbe/', include('dbe.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+from django.contrib import admin
+admin.autodiscover()
+urlpatterns += patterns('',
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+from django.conf.urls import *
+urlpatterns += patterns('blog.views',
+   (r"^$", "main"),
+)
