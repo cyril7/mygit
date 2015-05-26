46286c1b0d4b2267918f9d5422d84ddfa5c916ca
cyril7 <cyril7@users.noreply.github.com>
2015-05-26 17:35:42 +0800

    Create alias_in_etcprofile.sh
    more Productivity

---
+++ shell/alias_in_etcprofile.sh
@@ -0,0 +1,50 @@
+#### Productivity
+#alias makescript="fc -rnl | head -1 >"
+#alias c="clear"
+alias ls="ls --color=auto"
+alias ll="ls --color -al"
+alias grep='grep --color=auto'
+mcd() { mkdir -p "$1"; cd "$1";}
+cls() { cd "$1"; ls;}
+backup() { cp "$1"{,.bak};}
+md5check() { md5sum "$1" | grep "$2";}
+alias genpasswd="strings /dev/urandom | grep -o '[[:alnum:]]' | head -n 30 | tr -d '\n'; echo"
+alias histg="history | grep"
+alias ..='cd ..'
+alias ...='cd ../..'
+extract() {
+    if [ -f $1 ] ; then
+      case $1 in
+        *.tar.bz2)   tar xvjf $1     ;;
+        *.tar.gz)    tar xvzf $1     ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       unrar e $1     ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar xvf $1      ;;
+        *.tbz2)      tar xvjf $1     ;;
+        *.tgz)       tar xvzf $1     ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *)     echo "'$1' cannot be extracted via extract()" ;;
+         esac
+     else
+         echo "'$1' is not a valid file"
+     fi
+}
+
+
+### System info
+alias cmount="mount | column -t"
+alias tree="ls -R | grep ":$" | sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'"
+sbs(){ du -b --max-depth 1 | sort -nr | perl -pe 's{([0-9]+)}{sprintf "%.1f%s", $1>=2**30? ($1/2**30, "G"): $1>=2**20? ($1/2**20, "M"): $1>=2**10? ($1/2**10, "K"): ($1, "")}e';}
+alias meminfo='free -m -l -t'
+alias ps?="ps aux | grep"
+
+#### Network
+alias listen="lsof -P -i -n"
+alias port='netstat -tulanp'
+alias ipinfo="curl ifconfig.me && curl ifconfig.me/host"
+
+#### Funny
+alias busy="cat /dev/urandom | hexdump -C | grep \"ca fe\""
